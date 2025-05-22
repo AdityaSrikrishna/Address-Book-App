e2b920f6e94a6c83e7f93a419e888bd73051634b
Sri Krishna Aditya Garimella <gskaditya007@gmail.com>
2025-05-22 11:03:22 +0530

    UC 2: Create REST Controller with CRUD HTTP Operations

---
+++ src/main/java/com/example/address/dto/AddressBookDTO.java
@@ -0,0 +1,10 @@
+package com.example.address.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+    private String name;
+    private String email;
+    private String phone;
+}
---
+++ src/main/java/com/example/address/model/AddressBookModel.java
@@ -0,0 +1,11 @@
+package com.example.address.model;
+
+import lombok.Data;
+
+@Data
+public class AddressBookModel {
+    private int id;
+    private String name;
+    private String email;
+    private String phone;
+}
